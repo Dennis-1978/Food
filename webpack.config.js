e51f70b70fd694ec528d473c52190eb5abf367d1
Denis <d.khokhloff2011@yandex.ru>
2023-01-23 20:18:55 +0300

    the build directory has been changed

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
 	entry: './js/script.js',
 	output: {
 		filename: 'bundle.js',
-		path: path.resolve(__dirname, 'dist/js')
+		path: path.resolve(__dirname, 'js/')
 	},
 	watch: true,
 
